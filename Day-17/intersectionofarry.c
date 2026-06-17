2c5e753db48dac8e55c3481d4edfb426b629e525
CRAckbig4inonego <ramansrivastava571@gmail.com>
2026-06-17 16:41:06 +0530

    Day-17 assingnment is uploaded

---
+++ Day-17/intersectionofarry.c
@@ -0,0 +1,26 @@
+#include<stdio.h>
+int main(){
+printf("Enter number of elements in first array :");
+    int n1;
+scanf("%d",&n1);
+int a[n1];
+for(int i=0;i<n1;i++)
+scanf("%d",&a[i]);
+printf("Enter number of elements in second array :");
+int n2;
+scanf("%d",&n2);
+int b[n2];
+for(int i=0;i<n2;i++)
+scanf("%d",&b[i]);
+
+ for(int i=0;i<n1;i++){
+    for(int j=0;j<n2;j++){
+if(a[i]==b[j]){
+printf("%d ",a[i]);
+break;
+}
+}
+}
+
+return 0;
+}
