5522ccc6e73438cfd2cbca0273b534450e9f00c7
CRAckbig4inonego <ramansrivastava571@gmail.com>
2026-06-28 07:53:33 +0530

    Day-28 assignment is uploaded

---
+++ Day-28/bookaccount.c
@@ -0,0 +1,35 @@
+#include <stdio.h>
+int main()
+{
+    int accNo;
+    char name[50];
+    float balance, deposit, withdraw;
+
+    printf("Enter Account Number: ");
+    scanf("%d",&accNo);
+
+    printf("Enter Customer Name: ");
+    scanf("%s",name);
+
+    printf("Enter Initial Balance: ");
+    scanf("%f",&balance);
+
+    printf("Enter Deposit Amount: ");
+    scanf("%f",&deposit);
+
+    balance += deposit;
+
+    printf("Enter Withdrawal Amount: ");
+    scanf("%f",&withdraw);
+
+    if(withdraw <=balance)
+        balance -=withdraw;
+    else
+        printf("Insufficient Balance!\n");
+
+    printf("\n----- Account Details -----\n");
+    printf("Account Number : %d\n",accNo);
+    printf("Customer Name  : %s\n",name);
+    printf("Available Balance : %.2f\n",balance);
+    return 0;
+}
